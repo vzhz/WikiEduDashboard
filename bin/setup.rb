fe6c178baded97334cac09c074b59fb305450003
Sage Ross <ragesoss@gmail.com>
2018-01-17 12:38:56 -0800

    Start building out setup script

--- bin/setup.rb
+++ bin/setup.rb
@@ -22,9 +22,26 @@ end
 
 # Install dependencies
 
+run 'sudo apt update'
+run 'sudo apt install -y default-libmysqlclient-dev pandoc curl r-base'
+
 run 'gem install bundler' if `which bundler`.empty?
 run 'bundle install'
-run 'sudo apt update'
-run 'sudo apt install pandoc'
+
+if `nodejs -v`[0].to_i < 6
+  run 'curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -'
+  run 'sudo apt install -y nodejs'
+end
+
+if `which yarn`.empty?
+  run 'curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -'
+  run 'echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list'
+  run 'sudo apt update && sudo apt install -y yarn'
+end
+
+run 'yarn'
+run 'sudo yarn global add phantomjs-prebuilt'
+run 'sudo yarn global add bower'
+run 'bower install'
 
 puts 'ohai'
